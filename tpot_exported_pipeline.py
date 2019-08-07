af4d56fe443d524026e4c12cd1ea7ebf93e7bdf2
Thiagio Silva <thiago.sf.silva@unesp.br>
2019-08-07 11:59:19 +0100

    V.1.2

--- tpot_exported_pipeline.py
+++ tpot_exported_pipeline.py
@@ -1,25 +1,20 @@
 import numpy as np
 import pandas as pd
-from sklearn.decomposition import PCA
-from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import MaxAbsScaler, StandardScaler
-from tpot.builtins import OneHotEncoder
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import LinearSVC
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
 features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
-            train_test_split(features, tpot_data['target'].values, random_state=None)
+            train_test_split(features, tpot_data['target'].values, random_state=79)
 
-# Average CV score on the training set was:0.872977331199605
+# Average CV score on the training set was:0.9873238272534278
 exported_pipeline = make_pipeline(
-    StandardScaler(),
-    MaxAbsScaler(),
-    PCA(iterated_power=4, svd_solver="randomized"),
-    OneHotEncoder(minimum_fraction=0.2, sparse=False, threshold=10),
-    ExtraTreesClassifier(bootstrap=True, criterion="gini", max_features=0.4, min_samples_leaf=3, min_samples_split=8, n_estimators=100)
+    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
+    LinearSVC(C=0.01, dual=True, loss="squared_hinge", penalty="l2", tol=0.1)
 )
 
 exported_pipeline.fit(training_features, training_target)
